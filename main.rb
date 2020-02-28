809f771a37a29bd467f886d54041c71c109920a1
Oscar Mier <voscarmv@gmail.com>
2020-02-28 10:09:30 -0600

    Added my_each

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-puts 'Hello, World!'
+module Enumerable
+
+  def my_each
+    if block_given?
+      for i in (0..self.length-1) do
+        yield(self[i])
+      end  
+    else
+      self.to_enum
+    end
+  end
+
+end
+
+[1, 2, 3, 4].my_each {|x| puts "number: #{x}"}
